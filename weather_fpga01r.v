a3e7b59ba84ec655d4a7c9c6efb0b90121107ed6
Lisa Zhai <lisazhai1022@gmail.com>
2022-06-23 23:18:58 +0900

    write connection with FPGA

---
+++ weather_fpga01r.v
@@ -0,0 +1,13 @@
+module weather_fpga01r(
+    input CLOCK_50,
+	 input[1:0] KEY,
+    output [2:0] LED
+);
+    wire clock;
+    wire [2:0] out;
+
+	 pll pll(CLOCK_50, ~KEY[1], clock);
+    decision_tree tree1(clock, KEY[0], 5'b01010, 5'b00000, 
+                        5'b00010, 5'b00100, out);
+    assign LED = out[2:0];
+endmodule
